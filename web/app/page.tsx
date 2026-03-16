ef8b9191fb512bd7f2be96df72969508b6a67785
BudHamud <nigitart@gmail.com>
2026-03-16 13:40:26 -0300

    fix: vercel v7

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+export const dynamic = 'force-dynamic'
 
 import AnimeApp from '@/components/AnimeApp'
 
--- web/components/AnimeApp.tsx
+++ web/components/AnimeApp.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { useMemo, useState, useEffect } from "react";
 import Auth from "@/components/Auth";
 import AddToVaultModal from "@/components/AddToVaultModal";
